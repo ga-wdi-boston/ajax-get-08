11e8794e0993cde5dc73b4d08b19f20b36a93cfe
MishaHerscu <mishaherscu@gmail.com>
2016-05-25 14:23:56 -0400

    demo from class

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,15 @@
 'use strict';
 
-// const libraryApi = require('./library-api');
-// const ui = require('./ui');
+const libraryApi = require('./library-api');
+const ui = require('./ui');
+
+// handler
+const getBooksHandler = function(event){
+  event.preventDefault();
+  libraryApi.getBooks().done(ui.onSuccess);
+};
 
 // On document ready
 $(() => {
+  $('#get-books').on('click', getBooksHandler);
 });
