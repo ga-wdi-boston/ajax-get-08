d5794d59ef4749d75c6dd98c7e1ecf90dff43430
dvdpwll <dvdpwll456@gmail.com>
2016-07-20 13:58:07 -0400

    David Powell, bonus
    made it print the book from the show function onto the page.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,37 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+//import other js files
+const libraryApi = require('./library-api');
+const ui = require('./ui');
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+//get books array from localhost:3000
+const onGetBooks = function(event){
+  //stops the submit buttons automatic features
+  event.preventDefault();
+
+  //assign the value in the textbox to bookID
+  let bookID = $('.book-id').val();
+
+  if (!bookID) {
+    //get request
+    libraryApi.index()
+      //if success
+      .done(ui.onSuccess)
+      //if fail
+      .fail(ui.onError);
+  }
+  else {
+    //get request
+    libraryApi.show(bookID)
+      //if success
+      .done(ui.onSuccess)
+      //if fail
+      .fail(ui.onError);
+  }
+};
+
+//document ready statement
+$(()=>{
+  //run code on submit
+  $('#book-request').on('submit', onGetBooks);
+});
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,3 +1,26 @@
 'use strict';
 
-module.exports = true;
+//has data
+const onSuccess = function(data){
+  //true if user enters an exisiting id in the text box
+  if (data.book) {
+      console.log(data.book);
+      $('form').append('<p>ID: ' + data.book.id + '<br>' +
+                       'Title: ' + data.book.title + '<br>' +
+                       'Author: ' + data.book.author + '</p>');
+  }
+  else {
+    console.log(data.books);
+  }
+
+};
+
+//has error
+const onError = function(response){
+  console.error(response);
+};
+
+module.exports = {
+  onSuccess,
+  onError,
+};
